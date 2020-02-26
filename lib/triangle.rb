f7f7824679178f9304ffa499142b5bb63b7a8984
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-26 21:06:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,25 @@ class Triangle
     @z = z
   end
   
-  def kind
+  def kind()
+    if (@x <= 0) || (@y <= 0) || (@z <= 0)
+      raise TriangleError
+    elsif (@x+@y <= @z) || (@x+@z <= @y) || (@y+@z <= @x)
+      raise TriangleError
+    else
+      if (@x == @y) && (@y == @z)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
   
+  class TriangleError < StandardError
+    
+  end
   
 end
